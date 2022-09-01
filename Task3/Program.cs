60c9a86732bc22ca187e60953f39c2e553aad132
ala markach <drozdowa1958@gmail.com>
2022-09-01 09:03:57 +0400

    add Task003

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+Random rd = new Random();
+int[] array = new int[8];
+ Console.Write("[");
+ void PrintArray(int[] arr)
+ {
+  
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = rd.Next(1,100);
+    
+        if(i!=7) Console.Write( array[i]+ ", " );
+        else Console.Write(  array[i]  );
+    }
+    Console.Write("]");
+  }
+ PrintArray(array);
+
+
+
+
